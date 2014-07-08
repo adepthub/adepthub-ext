0e3cd305a13f0f65a6cbaa1b2062c348dc766407
Arve Knudsen <arve.knudsen@gmail.com>
2014-07-09 01:14:19 +0200

    Implement JSON converter for ContributionResult

--- adepthub-ext/src/main/scala/adepthub/models/ContributionResult.scala
+++ adepthub-ext/src/main/scala/adepthub/models/ContributionResult.scala
@@ -1,12 +1,19 @@
 package adepthub.models
 
+import adept.artifact.models.JsonSerializable
 import adept.repository.models.RepositoryName
 import adept.repository.models.Commit
-import com.fasterxml.jackson.core.JsonParser
+import com.fasterxml.jackson.core.{JsonGenerator, JsonParser}
 import adept.services.JsonService
 
-case class ContributionResult(repository: RepositoryName, commit: Commit, locations:
-Seq[String])
+case class ContributionResult(repository: RepositoryName, commit: Commit, locations: Seq[String])
+  extends JsonSerializable {
+  override def writeJson(generator: JsonGenerator): Unit = JsonService.writeJson({ generator: JsonGenerator =>
+    generator.writeStringField("repository", repository.value)
+    generator.writeStringField("commit", commit.value)
+    JsonService.writeStringArrayField("locations", locations, generator)
+  })
+}
 
 object ContributionResult {
   def fromJson(parser: JsonParser): ContributionResult = {
