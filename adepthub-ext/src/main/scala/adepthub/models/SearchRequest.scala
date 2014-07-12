be9c7cdc34d6a9c6f7dfe2ee051ac5de837b7732
Arve Knudsen <arve.knudsen@gmail.com>
2014-07-12 23:39:26 +0200

    Remove unused method

--- adepthub-ext/src/main/scala/adepthub/models/SearchRequest.scala
+++ adepthub-ext/src/main/scala/adepthub/models/SearchRequest.scala
@@ -11,13 +11,3 @@ case class SearchRequest(term: String, constraints: Set[Constraint]) {
     JsonService.writeArrayField("constraints", constraints, generator)
   })
 }
-
-object SearchRequest {
-  def readJson(is: InputStream): SearchRequest = {
-    JsonService.parseJson(is, Map(
-      ("term", _.getValueAsString),
-      ("term", JsonService.parseSet[Constraint](_, Constraint.fromJson))
-    ),
-      valueMap => SearchRequest(valueMap.getString("term"), valueMap.getSet[Constraint]("constraints")))._1
-  }
-}
