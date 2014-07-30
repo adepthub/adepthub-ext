9c34f3afe56233e7e00e90f648c424d856ea0316
Arve Knudsen <arve.knudsen@gmail.com>
2014-07-30 22:22:48 +0200

    Clean up code

--- adepthub-sbt/src/main/scala/adept/sbt/SbtUtils.scala
+++ adepthub-sbt/src/main/scala/adept/sbt/SbtUtils.scala
@@ -5,15 +5,14 @@ import sbt._
 object SbtUtils {
 
   def evaluateTask[A](key: TaskKey[A], ref: ProjectRef, state: State): A =
-    EvaluateTask(structure(state), key, state, ref, EvaluateTask defaultConfig state) match {
+    EvaluateTask(structure(state), key, state, ref, EvaluateTask.defaultConfig(state)) match {
       case Some((_, Value(a))) => a
-      case Some((_, Inc(inc))) => throw new Exception("Error evaluating task '%s': %s".format(key.key, Incomplete.show(inc.tpe)))
-      case None => throw new Exception("Undefined task '%s' for '%s'!".format(key.key, ref.project))
+      case Some((_, Inc(inc))) => throw new Exception(
+        s"Error evaluating task '${key.key}': ${Incomplete.show(inc.tpe)}")
+      case None => throw new Exception(s"Undefined task '${key.key}' for '${ref.project}'!")
     }
 
-  def extracted(state: State): Extracted = Project.extract(state)
-
-  def structure(state: State): BuildStructure = extracted(state).structure
+  def structure(state: State): BuildStructure = Project.extract(state).structure
 
   def currentProject(state: State) = Project.current(state)
 
@@ -24,8 +23,10 @@ object SbtUtils {
    * runtime extends (test)
    *
    */
-  def getAllExtendingConfig(logger: sbt.Logger, conf: sbt.Configuration, ivyConfigurations: Seq[sbt.Configuration]): Seq[sbt.Configuration] = {
-    def getAllExtendingConfig(conf: sbt.Configuration, ivyConfigurations: Seq[sbt.Configuration]): Seq[sbt.Configuration] = {
+  def getAllExtendingConfig(logger: sbt.Logger, conf: sbt.Configuration, ivyConfigurations:
+  Seq[sbt.Configuration]): Seq[sbt.Configuration] = {
+    def getAllExtendingConfig(conf: sbt.Configuration, ivyConfigurations: Seq[sbt.Configuration]):
+    Seq[sbt.Configuration] = {
 
       val current = ivyConfigurations.filter { descendant =>
         descendant.extendsConfigs.contains(conf)
--- adepthub-sbt/src/main/scala/adept/sbt/commands/ContributeCommand.scala
+++ adepthub-sbt/src/main/scala/adept/sbt/commands/ContributeCommand.scala
@@ -1,45 +1,30 @@
 package adept.sbt.commands
 
-import sbt.State
-import java.io.File
 import adept.AdeptHub
-import adept.ivy.scalaspecific.ScalaBinaryVersionConverter
-import adept.resolution.models._
-import adepthub.models._
-import adept.repository.metadata._
-import adept.repository.models._
-import adept.ext.models.Module
-import adept.ext.VersionRank
-import adept.ivy.IvyUtils
 import adept.lockfile.Lockfile
-import adept.sbt.AdeptDefaults
-import adept.sbt.SbtUtils
-import adept.ivy.IvyConstants
-import adept.ext.AttributeDefaults
-import adept.sbt.AdeptKeys
+import adept.sbt.{AdeptKeys, SbtUtils}
+import sbt.State
 
 object ContributeCommand {
   import sbt.complete.DefaultParsers._
-  import sbt.complete._
 
   def using(adepthub: AdeptHub) = {
-    ((token("contribute-imports")).map { _ =>
+    token("contribute-imports").map { _ =>
       new ContributeCommand(adepthub)
-    })
+    }
 
   }
 }
 
 class ContributeCommand(adepthub: AdeptHub) extends AdeptCommand {
   def execute(state: State): State = {
-    val logger = state.globalLogging.full
     val lockfiles = SbtUtils.evaluateTask(AdeptKeys.adeptLockfiles, SbtUtils.currentProject(state), state)
     val results = adepthub.contribute()
     lockfiles.foreach { case (conf, lockfileFile) =>
       val lockfile = Lockfile.read(lockfileFile)
       adepthub.writeLockfile(adept.Contribute.updateWithContributions(lockfile, results), lockfileFile)
     }
-    scala.reflect.io.Directory(adepthub.importsDir).deleteRecursively
+    scala.reflect.io.Directory(adepthub.importsDir).deleteRecursively()
     
     state
   }
